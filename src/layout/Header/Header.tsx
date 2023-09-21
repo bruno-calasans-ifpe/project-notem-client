51c1fc66984298b1069cfad0e1066c3080eaf13c
bruno-calasans-ifpe <brcl1@discente.ifpe.edu.br>
2023-09-21 16:46:10 -0300

    create home page's header

--- src/layout/Header/Header.tsx
+++ src/layout/Header/Header.tsx
@@ -1,7 +1,93 @@
-import { Container } from './header.style'
+import { Group, Button, Image, Anchor, Text } from '@mantine/core'
+import { IconArrowRight } from '@tabler/icons-react'
+import { Container, TopHeader, LogoArea, MenuArea, BottomHeader, WelcomeMsgArea, ObsArea } from './header.style'
 
 function Header() {
-  return <Container>Header</Container>
+  return (
+    <Container>
+      <TopHeader>
+        <LogoArea id="logo">
+          <Image
+            src="./imgs/notem-logotype.jpg"
+            alt="notem logo"
+            w={150}
+          />
+        </LogoArea>
+        <MenuArea id="menu">
+          <Group gap="xs">
+            <Button
+              variant="outline"
+              color="indigo"
+            >
+              <Text fw="bold">Login</Text>
+            </Button>
+            <Button
+              variant="outline"
+              color="indigo"
+            >
+              <Text fw="bold">Registrar</Text>
+            </Button>
+            <Button
+              variant="outline"
+              color="indigo"
+            >
+              <Text fw="bold">Sobre</Text>
+            </Button>
+          </Group>
+        </MenuArea>
+      </TopHeader>
+      <BottomHeader>
+        <WelcomeMsgArea>
+          <Text
+            id="msg"
+            size="xl"
+            fw="bold"
+            c="dark"
+          >
+            Vai curtir um dia de praia? Que tal aproveitar tudo o que há de melhor em termos de produtos e serviços, na
+            praia que você escolheu<Anchor href="#obs1">¹</Anchor>, sem ter que ficar procurando por eles? No NOTem você
+            encontra diversos vendedores dispostos a levar de tudo<Anchor href="#obs2">²</Anchor>, bem onde você está!
+          </Text>
+
+          <Button
+            color="teal"
+            size="md"
+            fullWidth={false}
+            rightSection={<IconArrowRight />}
+          >
+            <Text
+              size="lg"
+              fw="bold"
+            >
+              Registrar-se
+            </Text>
+          </Button>
+        </WelcomeMsgArea>
+        <ObsArea>
+          <Text
+            id="obs1"
+            size="sm"
+            fs="italic"
+            c="dark"
+            fw="bold"
+            ff="cursive"
+          >
+            1* dentro as praias disponíveis
+          </Text>
+          <Text
+            id="obs2"
+            size="sm"
+            fs="italic"
+            c="dark"
+            fw="bold"
+            ff="cursive"
+          >
+            2* dentro do que o vendedor tem a oferecer
+          </Text>
+        </ObsArea>
+      </BottomHeader>
+    </Container>
+  )
 }
 
 export default Header
--- src/layout/Header/header.style.ts
+++ src/layout/Header/header.style.ts
@@ -2,6 +2,65 @@
 import styled from 'styled-components'
 
 export const Container = styled.header`
-  /* height: 200px; */
   background-color: lightblue;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  padding: 20px;
+
+  /* parallax */
+  background-image: url('./imgs/beach2.svg');
+  background-size: cover;
+  background-attachment: fixed;
+  background-repeat: no-repeat;
+  background-position: left top;
+`
+
+export const TopHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+  justify-self: flex-start;
+`
+
+export const LogoArea = styled.div`
+  /* img {
+    border-radius: 50%;
+  } */
+`
+
+export const MenuArea = styled.nav`
+  /* span {
+    font-weight: bold;
+  } */
+`
+
+export const BottomHeader = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+  flex-grow: 1;
+  color: var(--letterColor1o1);
+`
+
+export const WelcomeMsgArea = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 20px;
+  flex-grow: 1;
+
+  #msg {
+    text-align: center;
+    font-family: var(--font3);
+    font-size: x-large;
+  }
+`
+
+export const ObsArea = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-self: flex-start;
+  margin-top: 60px;
 `
