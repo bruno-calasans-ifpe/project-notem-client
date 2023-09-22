f4a18236cde42a98c9492d6bf8e5941a34cefa14
bruno-calasans-ifpe <brcl1@discente.ifpe.edu.br>
2023-09-22 16:37:18 -0300

    rename ContentFix to fixContentWidth and move to styles folder

--- src/components/ContentFix/ContentFix.ts
+++
@@ -1,9 +0,0 @@
-import { css } from 'styled-components'
-
-const ContentFix = css`
-  max-width: var(--contentMaxWidth);
-  margin-left: auto;
-  margin-right: auto;
-`
-
-export default ContentFix
---
+++ src/styles/fixContentWidth.ts
@@ -0,0 +1,9 @@
+import { css } from 'styled-components'
+
+const fixContentWidth = (maxWidth: React.CSSProperties['width']) => css`
+  max-width: ${maxWidth || 'var(--contentMaxWidth)'};
+  margin-left: auto;
+  margin-right: auto;
+`
+
+export default fixContentWidth
