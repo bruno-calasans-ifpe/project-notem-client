7345066e4cce1f1d40385701f6b934762ed31252
bruno-calasans-ifpe <brcl1@discente.ifpe.edu.br>
2023-10-14 18:20:32 -0300

    create register route

--- src/App.tsx
+++ src/App.tsx
@@ -10,13 +10,19 @@ import Main from './layout/Main/Main'
 import Footer from './layout/Footer/Footer'
 
 // Views
+import Register from './view/Register/Register'
 
 function App() {
   return (
     <>
       <Header />
       <Main>
-        <Routes></Routes>
+        <Routes>
+          <Route
+            path="/register"
+            element={<Register />}
+          ></Route>
+        </Routes>
       </Main>
       <Footer />
     </>
