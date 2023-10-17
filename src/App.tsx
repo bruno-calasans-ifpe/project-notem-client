3423ea51a2606c61539b63b9943e7c733beecb1c
bruno-calasans-ifpe <brcl1@discente.ifpe.edu.br>
2023-10-17 16:39:53 -0300

    create menu on header for autenticated users

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@
 import { Routes, Route } from 'react-router'
 
 // Layout
+import { useEffect } from 'react'
 import Header from './layout/Header/Header'
 import Main from './layout/Main/Main'
 import Footer from './layout/Footer/Footer'
@@ -13,12 +14,23 @@ import useAuthStore from './store/useAuthStore'
 // Views
 
 function App() {
-  const { user } = useAuthStore((state) => state)
+  const { user, login } = useAuthStore((state) => state)
+
+  // mock user
+  useEffect(() => {
+    login({
+      name: 'Test User',
+      email: 'test@gmail.com',
+      accountType: 'cliente',
+    })
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
   return (
     <>
       <Header />
       <Main>
-        <Routes></Routes>
+        <Routes>{/* <Route></Route> */}</Routes>
       </Main>
       <Footer />
     </>
--- src/layout/Header/Header.tsx
+++ src/layout/Header/Header.tsx
@@ -1,14 +1,25 @@
-import { Group, Button, Image, Anchor, Text } from '@mantine/core'
-import { IconArrowRight } from '@tabler/icons-react'
+/* eslint-disable react/jsx-no-useless-fragment */
+import { Group, Button, Image, Anchor, Text, Menu } from '@mantine/core'
+import {
+  IconArrowRight,
+  IconSettings2,
+  IconUser,
+  IconLogout,
+  IconMenu2,
+  IconHistory,
+  IconStar,
+} from '@tabler/icons-react'
 import { useLocation } from 'react-router'
 import { Container, TopHeader, LogoArea, MenuArea, BottomHeader, WelcomeMsgArea, ObsArea } from './header.style'
+import useAuthStore from '../../store/useAuthStore'
 
 function Header() {
   const currentLocation = useLocation()
   const isHomePage = currentLocation.pathname === '/'
+  const { user } = useAuthStore()
 
   return (
-    <Container className={isHomePage ? '' : 'no-welcome-msg'}>
+    <Container className={isHomePage && !user ? '' : 'no-welcome-msg'}>
       <TopHeader id="top-header">
         <LogoArea id="logo">
           <Anchor href="/">
@@ -21,28 +32,93 @@ function Header() {
         </LogoArea>
         <MenuArea id="nav-menu">
           <Group gap="xs">
-            <Button
-              variant="outline"
-              color="indigo"
-            >
-              <Text fw="bold">Login</Text>
-            </Button>
-            <Button
-              component="a"
-              href="/register"
-              variant="outline"
-              color="indigo"
-            >
-              <Text fw="bold">Registrar</Text>
-            </Button>
-            <Button
-              component="a"
-              href="/about/us"
-              variant="outline"
-              color="indigo"
-            >
-              <Text fw="bold">Sobre</Text>
-            </Button>
+            {user ? (
+              // authenticated user
+              <>
+                <Menu
+                  shadow="md"
+                  position="bottom-end"
+                >
+                  <Menu.Target>
+                    <Button
+                      variant="subtle"
+                      color="indigo"
+                      leftSection={<IconMenu2 size={20} />}
+                    >
+                      <Text fw="bold">Menu</Text>
+                    </Button>
+                  </Menu.Target>
+                  <Menu.Dropdown>
+                    <Menu.Item
+                      leftSection={<IconUser size={20} />}
+                      component="a"
+                      href="/account"
+                    >
+                      Minha Conta
+                    </Menu.Item>
+
+                    <Menu.Item
+                      leftSection={<IconSettings2 size={20} />}
+                      component="a"
+                      href="/settings"
+                    >
+                      Configurações
+                    </Menu.Item>
+
+                    <Menu.Item
+                      leftSection={<IconHistory size={20} />}
+                      component="a"
+                      href="/history"
+                    >
+                      Histórico
+                    </Menu.Item>
+
+                    <Menu.Item
+                      leftSection={<IconStar size={20} />}
+                      component="a"
+                      href="/favorites"
+                    >
+                      Favoritos
+                    </Menu.Item>
+
+                    <Menu.Item
+                      color="red"
+                      leftSection={<IconLogout />}
+                      component="a"
+                      href="/logout"
+                    >
+                      Sair
+                    </Menu.Item>
+                  </Menu.Dropdown>
+                </Menu>
+              </>
+            ) : (
+              // non-authenticated user
+              <>
+                <Button
+                  variant="outline"
+                  color="indigo"
+                >
+                  <Text fw="bold">Login</Text>
+                </Button>
+                <Button
+                  component="a"
+                  href="/register"
+                  variant="outline"
+                  color="indigo"
+                >
+                  <Text fw="bold">Registrar</Text>
+                </Button>
+                <Button
+                  component="a"
+                  href="/about/us"
+                  variant="outline"
+                  color="indigo"
+                >
+                  <Text fw="bold">Sobre</Text>
+                </Button>
+              </>
+            )}
           </Group>
         </MenuArea>
       </TopHeader>
