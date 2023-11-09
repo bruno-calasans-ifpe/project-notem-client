640335b3bb07c9796f542f33b7654417a596209e
bruno-calasans-ifpe <brcl1@discente.ifpe.edu.br>
2023-11-08 21:12:52 -0300

    fix header text

--- src/layout/Header/Header.tsx
+++ src/layout/Header/Header.tsx
@@ -63,11 +63,11 @@ function Header() {
             fw="bold"
             c="dark"
           >
-            Vai curtir um dia de praia? Que tal aproveitar tudo o que há de
-            melhor em termos de produtos e serviços, na praia que você escolheu
-            <Anchor href="#obs1">*</Anchor>, sem ter que ficar procurando por
-            eles? No NOTem você encontra diversos vendedores dispostos a levar
-            de tudo<Anchor href="#obs2">**</Anchor>, bem onde você está!
+            Vai curtir um dia de praia? Que tal aproveitar tudo o que há de melhor em
+            termos de produtos e serviços, na praia que você escolheu
+            <Anchor href="#obs1">*</Anchor>, sem ter que ficar procurando por eles? No
+            NOTem você encontra diversos vendedores dispostos a levar de tudo
+            <Anchor href="#obs2">**</Anchor>, bem onde você está!
           </Text>
 
           <Button
